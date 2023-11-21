389bf3fb857915d1e1187f3dc2fb1a6ec7c82507
Khandakar Aryanul Haque <123832026+aryanul@users.noreply.github.com>
2023-11-22 05:01:51 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,143 @@
+
+alert("In this game the computer will show you the color to click and you have to click those but as your level increases you have to remember all the colors you have clicked in preious level in the respective pattern and then click on the color of current level. Enjoy :)")
+
+var buttonColors = ["red","blue","green","yellow"];
+var gamePattern= [];
+var userClickedPattern= [];
+var started = false;
+var level = 0;
+$(document).keypress(function() {
+    if (!started) {  
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
+$(".btn").click(buttonClick);
+
+function startOver(){
+    gamePattern=[];
+    userClickedPattern=[];
+    started= false;
+    level= 0;
+}
+
+function buttonClick(){
+    var userChosenColour = $(this).attr("id");
+    $("#"+userChosenColour).addClass("pressed")
+    setTimeout(function(){
+        $("#"+userChosenColour).removeClass("pressed");
+    },100);
+    playSound(userChosenColour);
+    userClickedPattern.push(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+}
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function () {
+              nextSequence();
+            }, 1000);
+            }
+    
+    } else {
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over")
+        },200);
+        playSound("wrong")
+        console.log("wrong");
+        $("h1").text("Game Over, Press Any Key to Restart")
+        startOver();
+    }
+}
+
+
+
+
+function playSound(name){
+    var audio = new Audio("./sounds/"+name+".mp3");
+    audio.play();
+}
+
+function nextSequence(){
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColor= buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function keyClick(){
+    //     var userChosenColour = $(this).attr("class");
+    //     $("."+userChosenColour).addClass("pressed");
+    //     setTimeout(function(){
+    //         $("."+userChosenColour).removeClass("pressed");
+    //     },100);
+    //     playSound(userChosenColour);
+    //     userClickedPattern.push(userChosenColour);
+    // }
+    // document.addEventListener("keypress", function(event){
+    //     switch (event.key) {
+    //         case "w":
+    //             var userChosenColour = $(this).attr("class");
+    //             $("."+userChosenColour).addClass("pressed");
+    //             setTimeout(function(){
+    //                 $("."+userChosenColour).removeClass("pressed");
+    //             },100);
+    //             playSound(userChosenColour);
+    //             userClickedPattern.push(userChosenColour);
+    //             break;
+    //         case "a":
+                
+    //             break;
+    //         case "s":
+                
+    //             break;
+    //         case "d":
+                
+    //             break;
+    
+    //         default: console.log(buttonInnerHTML);
+    //             break;
+    //     }
+    // });
